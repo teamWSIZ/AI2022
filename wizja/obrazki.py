1ff51c0b1c1e3c823e5a68fc1d6f77781f04ca99
Piotr Marecki <pmarecki.pro@gmail.com>
2022-04-02 14:25:22 +0200

    wyjanśnianie i ostrzenie obrazka

--- wizja/obrazki.py
+++ wizja/obrazki.py
@@ -1,8 +1,11 @@
-from PIL import Image
+from PIL import Image, ImageOps, ImageEnhance
 from PIL.Image import BICUBIC
 
 # pip install Pillow
 
 img = Image.open('pic.jpg')
-img = img.resize((64,64), resample=BICUBIC)
-img.show()
+igs = ImageOps.grayscale(img)
+# bright = ImageEnhance.Brightness(img).enhance(1.5)
+bright = ImageEnhance.Sharpness(img).enhance(1.5)
+# img = img.resize((64,64), resample=BICUBIC)
+bright.show()
