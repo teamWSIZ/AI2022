e4cc0246c65f8bc1b35ac874d1a0e9b61fd5256d
Piotr Marecki <pmarecki.pro@gmail.com>
2022-04-09 13:55:49 +0200

    rozpoznawanie obrazow 2d

---
+++ wizja/convnet1.py
@@ -0,0 +1,171 @@
+from time import sleep
+
+import torch
+from torch import nn, optim, tensor, Tensor
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+
+# from helpers import format_t
+# from data_gen_1 import *
+# from torch_helpers import *
+from podstawy.torch_helpers import shuffle_samples_and_outputs
+from wizja.generation.g1 import generate_sample
+
+
+class ConvNet(nn.Module):
+    """
+        Simple NN: input(N_IN) ---> flat(hid) ---> output (N_OUT)
+    """
+
+    def __init__(self, res, n_out):
+        super().__init__()
+        self.res = res
+        self.n_out = n_out
+        self.ch1 = 5
+        self.hid = 10
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=5, padding=2)  # pozostawia RES x RES
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # RES → RES/2 (64 → 32)
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=9, kernel_size=5, padding=2)  # pozostawia RES x RES
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # RES → RES/4 (32 → 16)
+
+        flat_size = 9 * (res // 4) ** 2  # 9 kanałów, RES/4 x RES/4
+        self.flat_input_size = flat_size
+
+        self.flat_in_h = nn.Linear(flat_size, self.hid, True)
+        self.flat_h_h = nn.Linear(self.hid, self.hid, True)
+        self.flat_h_out = nn.Linear(self.hid, n_out, True)
+
+    def forward(self, x):
+        x = x.view(-1, 3, self.res, self.res)
+        y = x.copy()
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = x.view(-1, self.flat_input_size)  # wypłaszczenie sygnału
+
+        z = torch.cat((x, y), 0)
+
+        x = F.relu(self.flat_in_h(x))
+        x = F.relu(self.flat_h_h(x))
+        x = F.relu(self.flat_h_out(x))
+        return x
+
+    def load(self, filename):
+        self.load_state_dict(torch.load(filename))
+        self.eval()
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+
+# TYP DANYCH I CPU/GPU
+dtype = torch.double
+# device = 'cpu'  # gdzie wykonywać obliczenia
+device = 'cuda'
+
+# GEOMETRIA SIECI
+RES = 128
+N_OUT = 2
+N_SAMPLES = 30  # liczba próbek treningowych z każdego typu
+
+# PROCES UCZENIA SIECI
+EPOCHS = 500
+REGENERATE_SAMPLES_EPOCHS = 150  # co tyle epok generujemy próbki treningowe na nowo
+RESHUFFLE_EPOCHS = 45
+BATCH_SIZE = 1000
+LR = 0.02
+MOMENTUM = 0.9
+
+# Net creation
+net = ConvNet(RES, n_out=N_OUT)
+net = net.double()
+
+if device == 'cuda':
+    net = net.cuda()  # cała sieć kopiowana na GPU
+
+
+# fixme: UWAGA!! Przy zmianie rozmiarów sieci nie można wczytywać stanu poprzedniej ↓↓.
+# net.load('saved_net_state.dat')
+
+
+def generate_sample_tensors() -> tuple[Tensor, Tensor]:
+    # ↓↓ to są listy pythona
+    samples_tcells = generate_sample(N_SAMPLES, 'generation/tcells', RES)
+    n_tcells = len(samples_tcells)
+    outputs_tcells = [(1, 0) for _ in range(n_tcells)]
+    n_bacter = n_tcells
+    samples_bacter = generate_sample(n_bacter, 'generation/bacteria', RES)
+    outputs_bacter = [(0, 1) for _ in range(n_bacter)]
+
+    sample = torch.cat((samples_tcells, samples_bacter), 0)
+    output = torch.cat((torch.tensor(outputs_tcells, dtype=dtype, device=device),
+                        torch.tensor(outputs_bacter, dtype=dtype, device=device)), 0)
+    if dtype == torch.double:
+        sample = sample.double()
+    if device == 'cuda':
+        sample = sample.cuda()
+
+    # zamiana próbek na torch.tensor (możliwa kopia do pamięci GPU)
+    # t_sample_ = tensor(sample, dtype=dtype, device=device)
+    # t_output_ = tensor(output, dtype=dtype, device=device)
+
+    return sample, output
+
+
+def split_to_batches(samples: Tensor, outputs: Tensor) -> tuple[list, list]:
+    return torch.split(samples, BATCH_SIZE), torch.split(outputs, BATCH_SIZE)
+
+
+# ####
+# Training setup
+loss_function = nn.MSELoss(reduction='mean')
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=MOMENTUM)  # będzie na GPU, jeśli gpu=True
+
+# Training
+print('starting training...')
+sleep(0.5)
+# Dane do wykresu ↓
+epo_ = []
+err_ = []
+
+t_sample, t_output = generate_sample_tensors()
+b_sample, b_output = split_to_batches(t_sample, t_output)
+
+for epoch in range(EPOCHS):
+    total_loss = tensor(0., device=device)
+    for (batch_s, batch_o) in zip(b_sample, b_output):
+        optimizer.zero_grad()
+        prediction = net(batch_s)
+        loss = loss_function(prediction, batch_o)
+
+        total_loss += loss
+        loss.backward()
+        optimizer.step()
+
+    # print(f'epoch={epoch}')
+    # Dodatkowe operacje w trakcie procesu uczenia
+    if epoch % 10 == 9:
+        mean_error = total_loss.item() / len(b_sample)
+        print(f' epoch:{epoch}, loss:{mean_error:.6f}')
+        epo_.append(epoch)
+        err_.append(mean_error)
+
+    if epoch % RESHUFFLE_EPOCHS == 0:
+        print('shuffle')
+        t_sample, t_output = shuffle_samples_and_outputs(t_sample, t_output)
+        b_sample, b_output = split_to_batches(t_sample, t_output)
+
+    if epoch % REGENERATE_SAMPLES_EPOCHS == 0:
+        print('generating new samples')
+        t_sample, t_output = generate_sample_tensors()
+        b_sample, b_output = split_to_batches(t_sample, t_output)
+
+# Optional result save
+net.save('saved_net_state.dat')
+print('net saved')
+plt.scatter(epo_, err_)
+plt.xlabel('epoch')
+plt.ylabel('error')
+plt.show()
---
+++ wizja/generation/g1.py
@@ -0,0 +1,82 @@
+from random import randint
+
+import torch
+from PIL import Image, ImageEnhance
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms as TS
+import torchvision.transforms.functional as F
+
+
+class SuperposeSign(object):
+    """
+    Klasa którą można użyć jako "Transformację" (jak: torchvision.transforms) w kompozycji transformacji
+    obrazów.
+    """
+    sign: Image
+    mask: Image
+
+    def __init__(self, sign_filename='sign.png'):
+        sign = Image.open(sign_filename).resize((64, 64))
+        self.sign = sign
+        self.mask = sign.copy().convert('L')  # 'cień'; "single channel image"
+        self.mask = ImageEnhance.Brightness(self.mask).enhance(4)  # maska jest typu ~0.4; (zbyt transparentna)
+        # self.mask.show()
+
+    def __call__(self, image):
+        """
+        Args:
+            image (PIL Image or numpy.ndarray): Image to be converted to tensor.
+        Returns:
+            PIL: Transformed image.
+        """
+        left = randint(0, 200)
+        top = randint(0, 200)
+        image.paste(im=self.sign, box=(left, top), mask=self.mask)  # naklejenie maski na obrazek
+        # box:  (left,upper) lub (left,upper,right,lower)
+        # mask: - tylko wybrany region będzie update'owany; tam gdzie jest 0 nie będzie zmiany
+        return image
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+def generate_transform(resolution=256):
+    """
+    Tworzy zestaw transformacji przygotowujących/randomizujących próbki.
+    Jeśli `sign_filename` jest podane, to ten obrazek będzie nakładany na tło poprzednich.
+    :return:
+    """
+    # podstawowe transformacje
+    tts = [TS.Resize((resolution, resolution)),
+           TS.RandomAffine(degrees=10, fill=(0, 0, 0), translate=(0.1, 0.1), scale=(0.5, 1.5), shear=(0, 0.2)),
+           TS.GaussianBlur(kernel_size=3),
+           TS.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=(-0.2,0.2)),
+           TS.ToTensor()]
+
+    return TS.Compose(tts)
+
+
+def generate_sample(img_count, directory_name, res):
+    """
+    :return: Tensor typu [80, 3, 128, 128], i.e. nr. obrazka, nr koloru, rząd, kolumna
+
+    Uwaga: liczba obrazków zawsze jest wielokrotnością liczby obrazków w folderze z których są pobierane.
+    """
+    t = generate_transform(resolution=res)
+    dataset = datasets.ImageFolder(directory_name, transform=t)
+    dataloader = DataLoader(dataset, batch_size=15, shuffle=True)  # adjust to number of pictures
+    res = None
+    while res is None or res.size()[0] < img_count:
+        for (images, classes) in dataloader:
+            # wizualne sprawdzanie wyników
+            # for i in images:
+            #     F.to_pil_image(i).show()
+            if res is None:
+                res = images
+            else:
+                res = torch.cat((res, images), 0)
+    return res
+
+
+if __name__ == '__main__':
+    generate_sample(6, 'tcells', 128)
