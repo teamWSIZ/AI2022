f8d8f3c75d6dcba130736efabf18586d4121e06d
Piotr Marecki <pmarecki.pro@gmail.com>
2022-06-04 01:49:34 +0200

    feat: add LSTM network working well for sin(x)

--- sequence_prediction/lstm_functions_trainer.py
+++ sequence_prediction/lstm_functions_trainer.py
@@ -3,6 +3,7 @@ from torch import nn, optim, tensor, Tensor
 
 from podstawy.helpers import format_list
 from sequence_prediction.functions_model import SequenceNet, model_sinus, model_tanh, model_lorentz
+from sequence_prediction.lstm_predictor import LSTM_Predictor
 from sequence_prediction.sample_generator import gen_samples
 
 dtype = torch.double
@@ -12,19 +13,18 @@ device = 'cpu'  # gdzie wykonywać obliczenia
 HISTORY_N = 40  # ile liczb wchodzi (długość listy -- historii na podstawie której przewidujemy)
 HID = 10  # ile neuronów w warstwie ukrytej
 
-N_SAMPLE = 10000
-BATCH_SIZE = 2500
-EPOCHS = 2000
+N_SAMPLE = 1000
+BATCH_SIZE = 250
+EPOCHS = 100
 LR = 0.01
 
 # Dane do uczenia sieci
-DX = 0.01
+DX = 0.05
 
 
 def generate_sample_tensors(x_from, x_to, dx, model_function, history_len, n_samples) -> tuple[Tensor, Tensor]:
     sample, output = gen_samples(x_from, x_to, dx, model_function, history_len, n_samples)
 
-    # zamiana próbek na tensory (możliwa kopia do pamięci GPU)
     t_sample = tensor(sample, dtype=dtype, device=device)
     t_output = tensor(output, dtype=dtype, device=device)
 
@@ -44,7 +44,7 @@ def split_to_batches(samples: Tensor, outputs: Tensor, batch_size) -> tuple[list
 def train(x_from, x_to, dx, model_function, history_len, hidden_neurons, load_net: bool, save_filename: str,
           learning_rate: float, device: str):
     # Create net, or load from a saved checkpoint
-    net = SequenceNet(history_len, hidden_neurons)
+    net = LSTM_Predictor(hidden_neurons, device)
     net = net.double()
     if load_net:
         net.load(save_filename)
@@ -53,7 +53,8 @@ def train(x_from, x_to, dx, model_function, history_len, hidden_neurons, load_ne
 
     # Training setup
     loss_function = nn.MSELoss(reduction='mean')
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)  # będzie na GPU, jeśli gpu=True
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+    # optimizer = optim.LBFGS(net.parameters(), lr=learning_rate)
 
     samples, outputs = generate_sample_tensors(x_from, x_to, dx, model_function, HISTORY_N, N_SAMPLE)
     b_sample, b_output = split_to_batches(samples, outputs, BATCH_SIZE)
@@ -61,22 +62,24 @@ def train(x_from, x_to, dx, model_function, history_len, hidden_neurons, load_ne
     # Training
     for epoch in range(EPOCHS):
         total_loss = 0
+
+        # for LBFGS
+        # def closure():
+        #     optimizer.zero_grad()
+        #     out = net(train_input)
+        #     loss = loss_function(out, train_target)
+        #     print('loss', loss.item())
+        #     loss.backward()
+        #     return loss
+        # optimizer.step(closure)
+
         for (batch_s, batch_o) in zip(b_sample, b_output):
             optimizer.zero_grad()
-            # print(batch_in)
             prediction = net(batch_s)
             prediction = prediction.view(-1)  # size: [5,1] -> [5] (flat, same as b_out)
             loss = loss_function(prediction, batch_o)
 
-            # if EPOCHS - epoch < 2:
-            #     # pokazujemy wyniki dla ostatnich
-            #     print('---------')
-            #     print(f'input: {batch_s.tolist()}')
-            #     print(f'pred:{format_list(prediction.tolist())}')
-            #     print(f'outp:{format_list(batch_o.tolist())}')
-
             total_loss += loss
-
             loss.backward()
             optimizer.step()
         if epoch % 20 == 0:
@@ -86,11 +89,14 @@ def train(x_from, x_to, dx, model_function, history_len, hidden_neurons, load_ne
     print('net saved')
 
 
-def predict(x_from, x_to, dx, model_function, history_len, hidden_neurons, saved_filename):
-    net = SequenceNet(history_len, hidden_neurons)
+def predict(x_from, x_to, dx, model_function, hidden_neurons, saved_filename, device):
+    net = LSTM_Predictor(hidden_neurons, device)
     net = net.double()
     if saved_filename != '':
         net.load(saved_filename)
+    if device == 'cuda':
+        net = net.cuda()
+
 
     history = [model_function(x_from + i * dx) for i in range(HISTORY_N)]  # początkowa historia
 
@@ -101,7 +107,6 @@ def predict(x_from, x_to, dx, model_function, history_len, hidden_neurons, saved
     while x < x_to:
         history_t = tensor([history], dtype=dtype, device=device)
         history_batch = torch.split(history_t, BATCH_SIZE)
-        # print(history_batch)
         nxt = net(history_batch[0])  # szukamy predykcji następnej wartości
         val = float(nxt[0][0])
 
@@ -121,8 +126,8 @@ def predict(x_from, x_to, dx, model_function, history_len, hidden_neurons, saved
 
 
 if __name__ == '__main__':
-    # train(x_from=2, x_to=8, dx=DX, model_function=model_lorentz, history_len=HISTORY_N, hidden_neurons=HID,
-    #       load_net=True, save_filename='save.dat', learning_rate=LR, device=device)
+    train(x_from=0, x_to=7, dx=DX, model_function=model_sinus, history_len=HISTORY_N, hidden_neurons=HID,
+          load_net=True, save_filename='save.dat', learning_rate=LR, device=device)
 
-    predict(x_from=10, x_to=11, dx=DX, model_function=model_sinus, history_len=HISTORY_N, hidden_neurons=HID,
-            saved_filename='save.dat')
+    predict(x_from=0, x_to=40, dx=DX, model_function=model_sinus, hidden_neurons=HID,
+            saved_filename='save.dat', device=device)
---
+++ sequence_prediction/lstm_predictor.py
@@ -0,0 +1,64 @@
+from torch import nn, optim, tensor, Tensor, zeros
+import torch.nn.functional as funct
+import torch
+
+
+
+class LSTM_Predictor(nn.Module):
+    """
+    Predictor using long short-term memory cells, based on
+    https://www.python-engineer.com/posts/pytorch-time-sequence/
+    """
+
+    def __init__(self, n_hidden=51, device='cpu'):
+        super(LSTM_Predictor, self).__init__()
+        print(f'Creating model with number of hidden params for LSTMs = {n_hidden}')
+        self.n_hidden = n_hidden
+        self.device = device
+
+        # lstm1 > lstm2 > linear
+        self.lstm1 = nn.LSTMCell(1, self.n_hidden)
+        self.lstm2 = nn.LSTMCell(self.n_hidden, self.n_hidden)
+        self.linear = nn.Linear(self.n_hidden, 1)
+
+    def forward(self, x: Tensor, future=0):
+        # x : (batch_size, input_size)
+
+        # dont do future prediction per default
+        outputs = []
+        n_samples = x.size(0)
+
+        h_t = zeros(n_samples, self.n_hidden, dtype=torch.double, device=self.device)  # hidden state for lstm1
+        c_t = zeros(n_samples, self.n_hidden, dtype=torch.double, device=self.device)  # initial cell state for lstm1
+
+        h_t2 = zeros(n_samples, self.n_hidden, dtype=torch.double, device=self.device)  # hidden state for lstm1
+        c_t2 = zeros(n_samples, self.n_hidden, dtype=torch.double, device=self.device)  # initial cell state for lstm2
+
+        output = None
+        for in_t in x.split(1, dim=1):
+            # (batch_size, 1)
+            # print(f'in_t.size={in_t.size()}')   # (2500, 1)
+            h_t, c_t = self.lstm1(in_t, (h_t, c_t))  # recursion: privide states, (hidden,cell) → new (hidden,cell)
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+        # print(f'output.size={output.size()}')
+        outputs.append(output)
+
+        # default: unused
+        for i in range(future):
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs.append(output)
+
+        outputs = torch.cat(outputs, dim=1)
+        return outputs
+
+    def load(self, filename):
+        self.load_state_dict(torch.load(filename))
+        self.eval()
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+
